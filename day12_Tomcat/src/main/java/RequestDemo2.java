5199e42deb1abfcca2cb27707ab11cffdcaadf88
lixiaoyang <islixiaoyang2021@163.com>
2022-07-08 12:58:55 +0800

    commit twice

--- day12_Tomcat/src/main/java/RequestDemo2.java
+++ day12_Tomcat/src/main/java/RequestDemo2.java
@@ -1,4 +1,8 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Classname RequestDemo2
@@ -6,6 +10,13 @@ import javax.servlet.http.HttpServlet;
  * @Created by 李晓阳
  */
 public class RequestDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
