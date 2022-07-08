64ebdf96fff135326ad30288b52654c4d4f70d2b
lixiaoyang <islixiaoyang2021@163.com>
2022-07-08 13:02:50 +0800

    change

--- day12_Tomcat/src/main/java/RequestDemo2.java
+++ day12_Tomcat/src/main/java/RequestDemo2.java
@@ -12,11 +12,11 @@ import java.io.IOException;
 public class RequestDemo2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+
     }
 }
